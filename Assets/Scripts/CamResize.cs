58e39e469d3a2e31df7103911a2c43bb7032ca04
VivatikPumpkinEater <vivatik.pumpkin.eater@gmail.com>
2022-09-07 16:41:04 +0300

    Исправлены слои отрисовки

--- Assets/Scripts/CamResize.cs
+++ Assets/Scripts/CamResize.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 
 public class CamResize : MonoBehaviour
 {
+    [SerializeField] private Vector2 _refResolution;
+    
     private void Awake()
     {
         Application.targetFrameRate = 120;
+    }
+    
+    private void Start()
+    {
+        float refAspect = _refResolution.x / _refResolution.y;
+        float scaleMultiplier = refAspect / Camera.main.aspect;
+        float newSize = Camera.main.orthographicSize * scaleMultiplier;
+
+        Camera.main.orthographicSize = newSize;
     }
 }
